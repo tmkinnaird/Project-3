6c362124396dfd8e1f66e1a2ac67480c89223374
Tyler Kinnaird <tmkinnaird87@gmail.com>
2021-01-06 19:07:12 -0700

    i quit

--- golf-app-api/server.js
+++ golf-app-api/server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 // const methodOverride  = require('method-overide');
 const mongoose = require ('mongoose');
 const db = mongoose.connection;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MONGODB_URI = process.env.MONGODB_URI; // <= Heroku
 
